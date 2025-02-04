8082f318c35dda74dd08a9d07a66fa14f8947fb2
BugHunter-X <askzenai@gmail.com>
2025-02-04 08:10:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+    console.log('Count incremented:', count);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
+  );
+}
+
+export default MyComponent;
